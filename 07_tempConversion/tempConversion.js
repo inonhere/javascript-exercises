d95f7b160cb85b39736d34f1c3a7a8913df68c23
Inon <pokemonxdloquendo@gmail.com>
2022-06-23 22:46:28 -0400

    ex 6 and 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
+const ftoc = function(x) {
+
+  var formula = (x - 32) * (5/9);
+  var roun = Math.round(formula * 10) / 10;
+    return roun;
 
 };
 
-const ctof = function() {
+const ctof = function(y) {
+  var formulita = (y) * (9/5) + 32;
+  var rondita = Math.round(formulita * 10) / 10;
+    return rondita
 
 };
 
