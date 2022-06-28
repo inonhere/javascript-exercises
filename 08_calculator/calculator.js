8636b3252f7a6a308c7eddb82a341d5d3500a217
Inon <pokemonxdloquendo@gmail.com>
2022-06-27 22:00:18 -0400

    excersises

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,26 +1,48 @@
-const add = function() {
+const add = function(x, y) {
+	return x + y
+};
+
+const subtract = function(a, b) {
+  return a - b
+};
+
+const sum = function(z) {
+let c = 0;
+for (let i = 0; i < z.length; i++){
+  c += z[i]
+}
+return c;
+}
+
+
+const multiply = function(v) {
+let q = 1;
+for (let i = 0; i < v.length; i++){
+  q *= v[i]
+}
+return q
+};
+
+const power = function(x, y) {
+  return x**y
 	
 };
 
-const subtract = function() {
+const factorial = function(x) {
+  let cuenta = x += 1
+  let z = []
+  for (let i = 0; i < cuenta; i++){
+          x -= 1
+          z.push(x)
+  } z.pop()
+  let c = 1;
+  for (let i = 0; i < z.length; i++){
+    c *= z[i]
+  }
+  return c;
+  }
+
 	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
-};
 
 // Do not edit below this line
 module.exports = {
--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (string) {
+    string = string //racecar or !racecar
+    x = string.split("").reverse().join(""); //racecar
+    y = string.split("").reverse().join(""); //racecar!
+    a = string.localeCompare(y);
 
+    if (x === string || array === array2)
+        {return true;}
+        else {
+            return false;
+        }
 };
 
 // Do not edit below this line
